e88dca3ad3f0615913c84723d14d3f359fbe00b9
lawalabdulrazaq <temitopelawal925@gmail.com>
2025-11-18 03:14:05 +0100

    modularize webpage from backend

--- weaveit/weaveit-generator/index.ts
+++
@@ -1,5 +0,0 @@
-// src/index.ts
-export { Config } from './config.ts';
-export { analyzeCode } from './codeAnalyzer.ts';
-export { generateSpeech } from './textToSpeech.ts';
-export { generateVideo } from './videoGenerator.ts';
--- weaveit/weaveit-generator/types.ts
+++
@@ -1,8 +0,0 @@
-// src/types.ts
-export interface TutorialResult {
-    tutorialText: string;
-    language: string;
-  }
-
-  export const __types = {};
-
